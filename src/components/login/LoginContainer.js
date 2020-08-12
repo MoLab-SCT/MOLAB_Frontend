ba8b340d2457568e9573bf82c730914b11789f06
Sa Eun Soo <tkdmstn0304@naver.com>
2020-08-13 06:50:10 +0900

    login page background color modify

--- src/components/login/LoginContainer.js
+++ src/components/login/LoginContainer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import "./LoginContainer.scss";
 import Logo from "../../image/logo.png";
