a0b06aeec3315393102fe5975c4de4a79aed3b6e
Sa Eun Soo <tkdmstn0304@naver.com>
2020-08-19 16:01:37 +0900

    modify addnouncePage style and add img resource

--- src/components/announce/AnnouncePage.js
+++ src/components/announce/AnnouncePage.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Menu from "../menu/Menu";
+import "./AnnouncePage.scss";
 
 function AnnouncePage() {
-  const [list, setList] = useState(null);
+  const [lists, setList] = useState(null);
   const [loading, setLoading] = useState(null);
   const [error, setError] = useState(null);
 
@@ -24,13 +26,25 @@ function AnnouncePage() {
 
   if (loading) return <div>로딩 중</div>;
   if (error) return <div>에러 발생</div>;
-  if (!list) return null;
+  if (!lists) return null;
 
   return (
-    <div>
-      {list.map((l) => (
-        <p>{l.username}</p>
-      ))}
+    <div className="molab_wrppaer">
+      <Menu fontColor="black" logoColor="black" />
+      <main>
+        <div className="announce_wrapper">
+          <div className="announce_wrapper_header">
+            <h1>MOLAB의 열린 참여</h1>
+            <p>다양한 활동을 찾아보고 참여해보세요!</p>
+            <input></input>
+          </div>
+          <div className="review_list">
+            {lists.map((list) => (
+              <p>{list.username}</p>
+            ))}
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
