44da09b8dfff55192be401e66feb52d8fd6eb8d8
Sa Eun Soo <tkdmstn0304@naver.com>
2020-10-05 22:51:31 +0900

    add base url in login function

--- src/components/login/LoginFunction.js
+++ src/components/login/LoginFunction.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 export const getUserInfo = async () => {
   const response = await axios({
+    baseURL: "http://localhost:3000/api",
     method: "get",
-    url: "api/auth/getUser",
+    url: "/auth/getUser",
   });
   return response;
 };
