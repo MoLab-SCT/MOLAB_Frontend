9a1c66d12d442c1229c8c796eb897cb46cc7e8c8
Sa Eun Soo <tkdmstn0304@naver.com>
2020-11-05 16:41:27 +0900

    add network section in main page

---
+++ src/components/main/networkSection/NetworkContainer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./NetworkContainer.scss";
+
+function NetworkContainer() {
+  return (
+    <div className="group">
+      <div className="group_header_area">
+        <span className="group_header">Network</span>
+      </div>
+      <div className="group_network_area">
+        <img src={require("./img/HYU_logo.png")} alt="한양대학교" />
+        <img src={require("./img/support1.png")} alt="시흥 리빙랩" />
+        <img src={require("./img/HYU_logo.png")} alt="한양대학교" />
+      </div>
+    </div>
+  );
+}
+
+export default NetworkContainer;
