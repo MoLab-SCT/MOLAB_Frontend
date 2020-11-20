b4ac2b89db9c603f72f86e7b8c47ed86b2b83565
Sa Eun Soo <tkdmstn0304@naver.com>
2020-11-20 23:48:44 +0900

    change the height of facebook section

--- src/components/main/today/TodayContainer.js
+++ src/components/main/today/TodayContainer.js
@@ -101,7 +101,7 @@ function FacebookPage() {
     <Iframe
       src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Flivinglab.siheung&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId"
       width="340px"
-      height="430px"
+      height="380px"
       style={{ border: "none", overflow: "hidden" }}
       scrolling="no"
       frameborder="0"
